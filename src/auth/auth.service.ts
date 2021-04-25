ed7ad45faa46aa361fcc0fdffedf059f532907ea
abukhtatyy <abukhtatyy@gmail.com>
2021-04-25 09:32:04 +0300

    поправил реализацию валидации логина

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Body, HttpException, HttpStatus, Injectable, Post } from "@nestjs/common";
+import { Body, HttpException, HttpStatus, Injectable, Post, UnauthorizedException } from "@nestjs/common";
 import { CreateUserDto } from "../users/dto/create-user.dto";
 import { UsersService } from "../users/users.service";
 import { JwtService } from "@nestjs/jwt";
@@ -13,11 +13,8 @@ export class AuthService {
   ) {}
 
   async login(dto: CreateUserDto) {
-    const user = await this.userService.getUserByEmail(dto.email);
-    if (!user) {
-      throw new HttpException('Пользователь с таким email не существует', HttpStatus.BAD_REQUEST);
-    }
-    const token = this.generateToken(user);
+    const user = await this.validateUser(dto);
+    const token = await this.generateToken(user);
     return token;
   }
 
@@ -28,14 +25,25 @@ export class AuthService {
     }
     const hashPassword = await bcrypt.hash(dto.password, 5);
     const user = await this.userService.create({...dto, password: hashPassword});
-    const token = this.generateToken(user);
+    const token = await this.generateToken(user);
     return token;
   }
 
-  async generateToken(user: User) {
+  private async generateToken(user: User) {
     const payload = {email: user.email, id: user.id, roles: user.roles}
     return {
       token: this.jwtService.sign(payload)
     };
+  }
+
+  private async validateUser(dto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(dto.email);
+    const passwordEquals = await bcrypt.compare(dto.password, user.password);
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({
+      message: 'Не корректный пользователь или пароль'
+    });
   }
 }
